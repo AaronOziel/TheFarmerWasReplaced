46ff2b37e5137d96e577c4650a6ff8d1177b7685
Aaron Oziel <aaron.oziel@gmail.com>
2024-06-04 14:56:27 -0700

    Renamed Planting.py to Simple Planting.py and small cleanup

---
+++ Simple Planting.py
@@ -0,0 +1,29 @@
+def planting(plant): # function
+    clear()
+    do_a_flip()
+    
+    while True:
+        for i in range(get_world_size()):
+            if can_harvest():            
+                harvest()
+                water()
+                plant()
+            move(South)
+        move(East)
+
+def plant_grass():
+    pass
+
+def plant_bushes():
+    plant(Entities.Bush)
+          
+def plant_carrots():
+    if get_ground_type() == Grounds.Turf:
+        till()
+    if num_items(Items.Carrot_Seed) == 0:
+        trade(Items.Carrot_Seed, world_area)
+    plant(Entities.Carrots)
+
+def plant_trees():
+    if (get_pos_x() % 2 == 0 and get_pos_y() % 2 == 1) or (get_pos_x() % 2 == 1 and get_pos_y() % 2 == 0):
+        plant(Entities.Tree)    
